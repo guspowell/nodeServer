839920301863825d36c299d90b76ced257273283
guspowell <augustuspowell@me.com>
2015-02-05 17:36:17 +0000

    first line of ajax written

--- server.js
+++ server.js
@@ -2,21 +2,44 @@ var express = require('express');
 var app = express();
 var http = require('http');
 var server = http.createServer(app);
-var port = 9999
+var port = 9999;
 
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+app.use('/stylesheets', express.static(__dirname + '/public/stylesheets'));
+app.use('/javascript', express.static(__dirname + '/public/javascript'));
 
-app.get('/', function(request, response) {
-  response.render('index', request.query);
+
+app.get('/', function(request, response){
+  response.header("Access-Control-Allow-Origin", "*")
+  response.render('github', request.query);
 });
 
-app.get('/reflect.json', function(request, response) {
-  response.render('reflect', {query: request.query});
-});
-
-server.listen(port, function() {
-  console.log("Server running on port" + port);
+server.listen(port, function(){
+  console.log('server running on port ' + port);
 });
 
 module.exports = server;
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// app.get('/', function(request, response) {
+//   response.render('index', request.query);
+// });
+//
+// app.get('/reflect.json', function(request, response) {
+//   response.render('reflect', {query: request.query});
+// });
+//
+// app.get('/github', function(request, response) {
+//   response.render('github', request.query);
+// });
--- test/homepage_features.js
+++ test/homepage_features.js
@@ -6,23 +6,51 @@ describe('homepage', function() {
     casper.start(host)
   });
 
-  it('hello world', function() {
-    casper.then(function() {
-      expect('body').to.have.text('Hello world');
-    });
-  });
-
-  it('should say hello gus', function() {
-    casper.thenOpen(host + '?name=Gus', function() {
-      expect('body').to.have.text('Hello Gus');
-    });
-  });
-
-
-  it('should give me some JSON love', function() {
-    casper.thenOpen(host + '/reflect.json?name=gus', function() {
-      expect('body').to.have.text('{"name":"gus"}');
-    });
-  });
-
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// it('hello world', function() {
+//   casper.then(function() {
+//     expect('body').to.have.text('Hello world');
+//   });
+// });
+//
+// it('should say hello gus', function() {
+//   casper.thenOpen(host + '?name=Gus', function() {
+//     expect('body').to.have.text('Hello Gus');
+//   });
+// });
+//
+//
+// it('should give me some JSON love', function() {
+//   casper.thenOpen(host + '/reflect.json?name=gus', function() {
+//     expect('body').to.have.text('{"name":"gus"}');
+//   });
+// });
+//
+// });
