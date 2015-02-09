81e4a293e815c075c5aaebd2dde771d542397c32
guspowell <augustuspowell@me.com>
2015-02-09 11:53:30 +0000

    test passing for params query name

--- 2/server.js
+++ 2/server.js
@@ -2,12 +2,20 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 
-server.listen(3000, function() {
-  console.log('Server listening on port 3000');
-});
+app.set('view engine', 'ejs');
+app.use('/stylesheets', express.static(__dirname + '/public/stylesheets'));
+app.use('javascripts', express.static(__dirname + '/public/javascripts'));
 
 app.get('/', function(request, response) {
-  response.send('hello world')
+  response.render('index', request.query);
+});
+
+app.get('/greeting', function(request, response) {
+  response.render('greeting', { name: 'gus' });
+});
+
+server.listen(3000, function() {
+  console.log('Server listening on port 3000');
 });
 
 module.exports = server;
--- 2/tests/homepage_features.js
+++ 2/tests/homepage_features.js
@@ -1,12 +1,26 @@
+var host = 'http://localhost:3000/';
+
 describe("homepage", function() {
 
   before(function() {
-    casper.start('http://localhost:3000/');
+    casper.start(host);
   });
 
   it('hello world', function() {
     casper.then(function() {
       expect('body').to.have.text('hello world');
+    });
+  });
+
+  it('greets', function() {
+    casper.thenOpen(host + '?name=gus&age=22', function() {
+      expect('body').to.have.text('hello gus')
+    });
+  });
+
+  it('should give me json format', function() {
+    casper.thenOpen(host + 'reflect.json?name=gus', function() {
+      expect('body').to.have.text('{"name":"gus"}');
     });
   });
 
